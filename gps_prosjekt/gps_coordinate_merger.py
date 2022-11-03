4206b5ec2c0a20ea27c2abd4e673bdd15c48ab90
Oskar Veggeland <oveggeland@gmail.com>
2022-11-02 15:41:52 -1000

    fix bug with gps time column index

--- gps_prosjekt/gps_coordinate_merger.py
+++ gps_prosjekt/gps_coordinate_merger.py
@@ -7,7 +7,7 @@ import numpy as np
 import glob
 import os
 
-def merge_files(folder, gps_delimiter=";"):
+def merge_files(folder, gps_delimiter=";", gps_col=3):
     try:
         os.mkdir(os.path.join("target", os.path.basename(folder)))
     except:
@@ -21,7 +21,7 @@ def merge_files(folder, gps_delimiter=";"):
     # Create position ground truth object pGT
     gps_data = pd.read_csv(gps_file, sep=gps_delimiter)
 
-    gps_times = gps_data.iloc[:, 2].to_numpy()
+    gps_times = gps_data.iloc[:, gps_col].to_numpy()
     gps_x = gps_data.iloc[:, 0].to_numpy()
     gps_y = gps_data.iloc[:, 1].to_numpy()
 
@@ -30,10 +30,14 @@ def merge_files(folder, gps_delimiter=";"):
     s = gps_times % 100
     gps_total_seconds = s + 60*m + 3600*h
 
+    print("GPS time interval:", gps_total_seconds.min(), gps_total_seconds.max())
+
     # Read datafile and interpolate gps coordinates
     sonar_data = pd.read_csv(data_file, delim_whitespace=True)
 
     sonar_total_seconds = sonar_data["HOUR"].to_numpy()*3600 + sonar_data["MINUTE"].to_numpy()*60 + sonar_data["SECOND"].to_numpy()
+    print("Data time interval:", sonar_total_seconds.min(), sonar_total_seconds.max())
+
     sonar_data['CDP_X'] = np.interp(sonar_total_seconds, gps_total_seconds, gps_x)
     sonar_data['CDP_Y'] = np.interp(sonar_total_seconds, gps_total_seconds, gps_y)
 
@@ -41,5 +45,5 @@ def merge_files(folder, gps_delimiter=";"):
 
 
 if __name__ == "__main__":
-    merge_files("porsgrunn", gps_delimiter=";")
-    merge_files("skoklevann", gps_delimiter=",")
+    merge_files("porsgrunn", gps_delimiter=";", gps_col=2)
+    merge_files("skoklevann", gps_delimiter=",", gps_col=3)
